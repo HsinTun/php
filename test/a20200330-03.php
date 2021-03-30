aa82391639e0e6617d9bbe1d1db5dafaf2dd67d0
HsinTun <orange3130873@gmail.com>
2021-03-30 12:18:34 +0800

    first commit

---
+++ test/a20200330-03.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$a="mimi so sweet";
+echo "hi, $a";
+echo '<br>';
+echo 'hi, $a';
+echo '<br>';
+echo"hi,$a 132有空格"; //有空格
+echo '<br>';
+echo"hi,{$a}132";
+echo '<br>';
+echo"hi, ${a}132";
+?>
+</body>
+</html>
